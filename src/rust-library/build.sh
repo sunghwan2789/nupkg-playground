cd78e19f8658ec58999fb872a441a298541e8ebb
Sunghwan Bang <sunghwan2789@gmail.com>
2025-05-21 14:50:18 +0900

    disable linux lib

--- src/rust-library/build.sh
+++ src/rust-library/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-targets="x86_64-unknown-linux-gnu aarch64-unknown-linux-gnu x86_64-pc-windows-gnu"
+# targets="x86_64-unknown-linux-gnu aarch64-unknown-linux-gnu x86_64-pc-windows-gnu"
+targets="x86_64-pc-windows-gnu"
 
 for target in $targets
 do
